d3b6765eef3054506cbbac47a2e433d1f5692079
railsonrames <rames.dev@outlook.com>
2020-01-26 23:42:11 -0300

    Mudando o menu lateral.

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header>
       <q-toolbar>
         <q-btn
           flat
@@ -10,12 +10,64 @@
           icon="menu"
           aria-label="Menu"
         />
-
-        <q-toolbar-title>Todo List</q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
+
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{ dataDoDia }}</div>
+      </div>
+      <q-img src="statics/pantheon.jpg" class="header-image absolute-top" />
     </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above :width="250" :breakpoint="700">
+      <q-scroll-area
+        style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd"
+      >
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="inbox" />
+            </q-item-section>
+
+            <q-item-section>Inbox</q-item-section>
+          </q-item>
+
+          <q-item active clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="star" />
+            </q-item-section>
+
+            <q-item-section>Star</q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="send" />
+            </q-item-section>
+
+            <q-item-section>Send</q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="drafts" />
+            </q-item-section>
+
+            <q-item-section>Drafts</q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img class="absolute-top" src="statics/pantheon.jpg" style="height: 192px">
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+          <div class="text-weight-bold">Razvan Stoenescu</div>
+          <div>@rstoenescu</div>
+        </div>
+      </q-img>
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -24,6 +76,8 @@
 </template>
 
 <script>
+import { date } from "quasar";
+
 export default {
   name: "MyLayout",
 
@@ -31,6 +85,21 @@ export default {
     return {
       leftDrawerOpen: false
     };
+  },
+  computed: {
+    dataDoDia() {
+      let timeStamp = Date.now();
+      return date.formatDate(timeStamp, "dddd D MMMM");
+    }
   }
 };
 </script>
+
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
