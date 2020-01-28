9948074e542f00b311b3a21b3ac2063c3ee126f5
railsonrames <rames.dev@outlook.com>
2020-01-28 20:26:16 -0300

    Adicionando q-list para os itens do todo.

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,6 +1,10 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Ajuda!</h1>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Ajuda!</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nulla sapien, tincidunt tempor ultrices eu, porttitor vitae augue. Quisque vitae magna pulvinar, dictum urna at, dictum felis. Integer at pharetra arcu, sed hendrerit ex. Nam mollis purus interdum vestibulum cursus. Nam consequat porta erat ac bibendum. In hac habitasse platea dictumst. Vestibulum dignissim congue ligula et rutrum. Fusce lacus lacus, tristique sed urna eget, ornare ultricies nisi. Nulla tristique ornare est ut laoreet. Quisque tempor lobortis mauris at viverra. Praesent iaculis mauris a auctor iaculis. Donec ac massa et elit ultrices tempus eget vitae nisl. Sed in ante ex. Vivamus pulvinar rhoncus quam, eu iaculis erat malesuada fermentum.</p>
+    <p>Fusce quis libero nec est convallis elementum quis vitae elit. Sed molestie, felis vitae placerat convallis, libero sem rhoncus erat, at commodo felis mauris non leo. Mauris condimentum pulvinar dictum. Duis ac hendrerit dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras cursus mauris nec lectus porta consequat. Sed eu nibh in sapien elementum finibus. Ut nec massa sem. Phasellus ullamcorper erat sed sapien elementum congue. Suspendisse ullamcorper semper nunc nec rutrum. Aenean in risus sodales, faucibus ligula a, placerat risus. Vestibulum finibus lectus at ullamcorper vestibulum. Sed commodo urna at diam tincidunt posuere.</p>
+    <p>Morbi bibendum ante ut risus rhoncus, sit amet posuere turpis interdum. Praesent varius vestibulum porta. Integer et varius nunc, in dapibus velit. Vestibulum ac ipsum tempor, ultrices felis sit amet, efficitur leo. Nam sed facilisis tellus, eu aliquet nunc. Quisque rhoncus nisl turpis, at dictum sapien tempor non. Quisque quis arcu ut diam sodales molestie iaculis id mauris.</p>
+    <p></p>
   </q-page>
 </template>
 
